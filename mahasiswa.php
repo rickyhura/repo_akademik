6cb3c614a7286381bd0563e6b4637f5498a7ac22
rickyhura <brilianricky23@gmail.com>
2025-03-10 11:47:09 +0700

    Update mahasiswa.php
    buat modul mahasiswa

--- mahasiswa.php
+++ mahasiswa.php
@@ -1,1 +1,15 @@
-
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Data Mahasiswa</title>
+</head>
+<body>
+    <h6>Data Mahasiswa AMP</h6>
+    <hr>
+    <h6>NIM : 23110002</h6>
+    <h6>NAMA : BRILIAN RICKY HURA</h6>
+    <a href="index.php">Kembali</a>
+    
+</body>
+</html>
